646778a6ece09bfaaafd7785bda0f21f0a060efe
Mitchell-Chatterjee <mitchell.chatterjee@gmail.com>
2022-11-10 12:02:54 -0500

    Added basic data distribution support

--- SupportClasses/EnvironmentSetup.py
+++ SupportClasses/EnvironmentSetup.py
@@ -1,9 +1,25 @@
 import torch
 import torchvision
+from itertools import permutations
 
 """
 The following methods are to be used to download and distribute the dataset among the various nodes.
 """
+
+DATA_DISTRIBUTION = [None, None, (0.25, 0.75), (0.25, 0.25, 0.50), (0.15, 0.15, 0.15, 0.55)]
+
+
+def data_distribution(number_of_nodes):
+    """
+    This method returns a list of tuples containing the data distribution per-client.
+    :param number_of_nodes: The number of devices in the federated model.
+    :return: A list of tuples containing all unique permutations of the size number_of_nodes.
+    """
+    return_list = []
+    for elem in permutations(DATA_DISTRIBUTION[number_of_nodes], number_of_nodes):
+        if elem not in return_list:
+            return_list.append(elem)
+    return return_list
 
 
 def download_mnist(train_batch_size=25, test_batch_size=100, num_workers=2):
